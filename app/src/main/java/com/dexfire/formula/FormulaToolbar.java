28988cf37b93d180a89b0eabbd8d6c5c1b5615a1
dexfire <275176629@qq.com>
2018-05-16 11:28:45 +0800

    添加key

---
+++ app/src/main/java/com/dexfire/formula/FormulaToolbar.java
@@ -0,0 +1,16 @@
+package com.dexfire.formula;
+
+public class FormulaToolbar
+{
+	/**
+	 *		引导界面  （）
+	 *		（可左右滑动）
+	 *		α   （列表展开）
+	 *		operaters  (/)
+	 *		lim
+	 *		{}
+	 *		()
+	 *		颜色
+	 *
+	 */
+}
